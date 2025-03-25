2d3dcd60d047985f08305cc915edaaaa8d09b2eb
jcampos-94 <jcampos.94@hotmail.com>
2025-03-25 18:57:33 -0500

    Messages and Login

--- server.js
+++ server.js
@@ -12,7 +12,31 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const utilities = require("./utilities/");
+const session = require("express-session");
+const pool = require('./database/');
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next();
+})
 
 /* ***********************
  * View Engine and Templates
@@ -31,6 +55,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// Accounts route
+app.use("/account", accountRoute);
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
